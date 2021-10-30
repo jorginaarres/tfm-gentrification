ec3fefc9242f8b0d8fcb8f119b3eee890425fc09
Alberto <algimeneza@gmail.com>
2021-10-30 20:30:09 +0200

    Add comments on main

--- __main__.py
+++ __main__.py
@@ -14,12 +14,14 @@ if __name__ == '__main__':
 
     # 1. Load data from sources
     if 'raw' in config['steps']:
+        # 1.1 Load data
         data_raw = load_data(config['data_source'], 'raw')
-
-        # 2. get basic info for each dataframe (nulls, size, etc.),
-        # clean (remove blank spaces, NA's, weird values...) and transform
-        # (filters, aggregates, etc.)
+        # 1.2 Transform input datasets filtering necessary rows and renaming
+        # columns. Discretize some column values.
         data_l1 = transform(data_raw, config)
+        # 1.3 Clean data: Trim strings and detect format issues..
+        pass
+        # 1.4 Print dataset info: size, columns, summary, NAs
         save_to_csv_l1(data_l1, config['l1_save_path'])
 
 
