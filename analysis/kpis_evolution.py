93025e9b4d9c9c1f6c8604107c1dabb98b02b3e6
Jorgina Arres Cardona <jorginaarrescardona@Jorginas-personal-MacBook-Pro.local>
2023-12-11 16:36:47 +0100

    Update kpis from 2015 to 2021 and add 2 age and studies

--- analysis/kpis_evolution.py
+++ analysis/kpis_evolution.py
@@ -5,27 +5,27 @@ import numpy as np
 def kpi_evolution(df: pd.DataFrame):
     results = {}
     df_2015 = df[df['anyo'] == 2015]
-    df_2022 = df[df['anyo'] == 2022]
-    variables = df.columns[4:13]
+    df_2021 = df[df['anyo'] == 2021]
+    variables = df.columns[4:15]
     for var in variables:
         var_2015 = var + '_2015'
-        var_2022 = var + '_2022'
+        var_2021 = var + '_2021'
         df_2015_var = df_2015[['nom_barrio', var]]
         df_2015_var = df_2015_var.rename(columns={var: var_2015})
-        df_2022_var = df_2022[['nom_barrio', var]]
-        df_2022_var = df_2022_var.rename(columns={var: var_2022})
-        df_diff = pd.merge(df_2015_var, df_2022_var, on='nom_barrio',
+        df_2021_var = df_2021[['nom_barrio', var]]
+        df_2021_var = df_2021_var.rename(columns={var: var_2021})
+        df_diff = pd.merge(df_2015_var, df_2021_var, on='nom_barrio',
                            how='inner')
 
-        df_diff['diff'] = np.where(df_diff[var_2015] <= df_diff[var_2022],
-                                   df_diff[var_2022] - df_diff[var_2015],
-                                   df_diff[var_2022] - df_diff[var_2022])
+        df_diff['diff'] = np.where(df_diff[var_2015] <= df_diff[var_2021],
+                                   df_diff[var_2021] - df_diff[var_2015],
+                                   df_diff[var_2021] - df_diff[var_2021])
 
         df_diff['diff_perc'] = np.round(100*(df_diff['diff'] / df_diff[
             var_2015]), 2)
 
         df_diff['evolution'] = np.where(
-            df_diff[var_2015] <= df_diff[var_2022],
+            df_diff[var_2015] <= df_diff[var_2021],
             '+' + df_diff['diff_perc'].astype(str) + '%',
             '-' + df_diff['diff_perc'].astype(str) + '%'
         )
